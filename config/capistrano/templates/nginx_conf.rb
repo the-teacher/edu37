e15356117f459059b00b702e7775ffa46a51ecda
Ilya N. Zykin <zykin-ilya@ya.ru>
2013-11-09 20:27:50 +0400

    nginx_config

--- config/capistrano/templates/nginx_conf.rb
+++ config/capistrano/templates/nginx_conf.rb
@@ -3,8 +3,8 @@ upstream <%= socket_name %> {
 }
 
 server{
-  listen 7080;
-  server_name localhost <%= site_name %> www.<%= site_name %>;
+  listen 80;
+  server_name <%= site_name %> *.<%= site_name %>;
   root <%= current_path %>/public;
   
   location / {
